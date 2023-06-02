bce6933e2132512a7c91d486cca2b685359fccef
Rodrigo Perez <rodrigo.alejandro.perez.104@gmail.com>
2023-06-02 13:35:27 -0300

    actualizado

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,31 +1,7 @@
 const server = require('./app')
 const PORT = 3001;
+const { conn } = require('./DB_connection');
+ 
+ conn.sync({ force: true });//para dropear o eliminar cada vez que se reinicie el servidor
 
-
-server.use(express.json())
-server.use(morgan('dev'));
-
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header(
-       'Access-Control-Allow-Headers',
-       'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    res.header(
-       'Access-Control-Allow-Methods',
-       'GET, POST, OPTIONS, PUT, DELETE'
-    );
-    next();
- });
-
- server.use('/rickandmorty', router)
-
-server.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
-
-
-
-
-
-
-// res.setHeader('Access-Control-Allow-Origin', '*'); /*esta linea lo que hace es que le damos permiso al front-end de que me pueda hacer peticiones*/
+server.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
